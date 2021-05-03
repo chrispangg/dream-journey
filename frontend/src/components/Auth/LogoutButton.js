c9b35e8509fd12900b4de9fbd25a9ca4542a9b10
JojoDong-99 <jojodong99@gmail.com>
2021-05-03 18:05:22 +1200

    add user's picture

--- frontend/src/components/Auth/LogoutButton.js
+++ frontend/src/components/Auth/LogoutButton.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import {Button} from "@material-ui/core";
+import Avatar from '@material-ui/core/Avatar';
 
 const LogoutButton = () => {
-  const { logout } = useAuth0();
+  const { logout, user } = useAuth0();
+  const { picture } = user;
+
   return (
+      <>
+      <Avatar src={picture} />
       <Button style={{color: 'white'}}
           onClick={() =>
               logout({
@@ -14,6 +19,7 @@ const LogoutButton = () => {
       >
         Log Out
       </Button>
+      </>
   );
 };
 
